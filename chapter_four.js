33c97a63090aa5048182f007481d6f76eab42265
David Pal <paldavid2030@gmail.com>
2019-06-07 11:07:00 +0300

    add reverse array

--- chapter_four.js
+++ chapter_four.js
@@ -26,3 +26,24 @@ function sum(range) {
   return range.reduce((a, b) => a + b);
 }
 
+//reverse array
+
+const testArray = [1, 2, 3, 4, 5, 6]
+
+function reverseArray(array) {
+  let reversedArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArray.push(array[i]);
+  }
+  return reversedArray;
+}
+
+function reverseArrayInPlace(array) {
+const arrayHalf = Math.floor(array.length/2);
+for (let i = 0; i < arrayHalf; i++) {
+  let old = array[i];
+  array[i] = array[array.length - 1 - i];
+  array[array.length - 1 - i] = old;
+}
+return array;
+}
