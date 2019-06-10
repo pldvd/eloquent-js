a2d75757ba0f01b3bc547a3b320b491e2805fc86
David Pal <paldavid2030@gmail.com>
2019-06-10 15:14:43 +0300

    add won loop

--- chapter_five.js
+++ chapter_five.js
@@ -6,3 +6,15 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 
 console.log(arrays.reduce((a, b) => a.concat(b)));
 
+//your own loop
+
+function loop(value, testFunc, updateFunc, bodyFunc) {
+  if (testFunc(value)) {
+    bodyFunc(value);
+    return loop(updateFunc(value), testFunc, updateFunc, bodyFunc)
+  } else {
+    return;
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
