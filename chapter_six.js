03223f0c99e62b495c5a192845235840c2ba4561
David Pal <paldavid2030@gmail.com>
2019-06-21 10:23:14 +0300

    add groups

--- chapter_six.js
+++ chapter_six.js
@@ -23,3 +23,36 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
+
+//groups 
+
+class Group {
+  constructor() {
+    this.members = [];
+  }
+  static from(iterable) {
+    const newGroup = new Group()
+    newGroup.members = iterable;
+    return newGroup;
+  }
+  add(el) {
+    if (!this.members.includes(el)) this.members.push(el);
+    else return;
+  }
+  has(el) {
+    return this.members.includes(el);
+  }
+  delete(el) {
+    this.members = this.members.filter((i) => i != el);
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
