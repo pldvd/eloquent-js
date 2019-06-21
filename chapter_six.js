9532f7cfc6cd8c1d804d0fd837ed508791b4e1f6
David Pal <paldavid2030@gmail.com>
2019-06-21 09:53:07 +0300

    add a vector type

--- chapter_six.js
+++ chapter_six.js
@@ -1,2 +1,25 @@
 // a vector type
 
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(otherVector) {
+    return new Vec(this.x + otherVector.x, this.y + otherVector.y);
+  }
+  minus(otherVector) {
+    return new Vec(this.x - otherVector.x, this.y - otherVector.y);
+  }
+
+  get length() {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
