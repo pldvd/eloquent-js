67aa721c6b8da974db0e687f731cbb2a23de7793
David Pal <paldavid2030@gmail.com>
2019-06-24 16:06:29 +0300

    add build graph function

--- chapter_seven_robot_project.js
+++ chapter_seven_robot_project.js
@@ -2,11 +2,30 @@
 // the description of the project can be found here: https://eloquentjavascript.net/07_robot.html
 
 const roads = [
-  "Alice's House-Bob's House",   "Alice's House-Cabin",
-  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Alice's House-Bob's House", "Alice's House-Cabin",
+  "Alice's House-Post Office", "Bob's House-Town Hall",
   "Daria's House-Ernie's House", "Daria's House-Town Hall",
   "Ernie's House-Grete's House", "Grete's House-Farm",
-  "Grete's House-Shop",          "Marketplace-Farm",
-  "Marketplace-Post Office",     "Marketplace-Shop",
-  "Marketplace-Town Hall",       "Shop-Town Hall"
+  "Grete's House-Shop", "Marketplace-Farm",
+  "Marketplace-Post Office", "Marketplace-Shop",
+  "Marketplace-Town Hall", "Shop-Town Hall"
 ];
+
+function buildGraph(from, to) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (!graph[from]) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of roads.map(r => r.split('-'))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+console.log(roadGraph);
