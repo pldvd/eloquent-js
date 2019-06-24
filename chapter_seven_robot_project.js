9584b0e94a90ecdfccd3163b7c53d861d55a8398
David Pal <paldavid2030@gmail.com>
2019-06-24 15:50:06 +0300

    add roads array

--- chapter_seven_robot_project.js
+++ chapter_seven_robot_project.js
@@ -1,3 +1,12 @@
 // a robot
 // the description of the project can be found here: https://eloquentjavascript.net/07_robot.html
 
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
