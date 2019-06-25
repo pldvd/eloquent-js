76dcf4443176567cf9e841216c186e1984ceabf5
David Pal <paldavid2030@gmail.com>
2019-06-25 12:53:56 +0300

    add compareRobots()

--- chapter_seven_robot_project.js
+++ chapter_seven_robot_project.js
@@ -105,3 +105,12 @@ function goalOrientedRobot({currentPlace, parcels}, route) {
 }
 
 runRobot(DeliveryState.random(), goalOrientedRobot, []);
+
+
+// measuring a robot
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+  
+}
+
+compareRobots(routeRobot, [], goalOrientedRobot, []);
