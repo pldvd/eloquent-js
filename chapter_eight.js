35392fc7eb8467ecfaf407a9042e95210a2a892b
David Pal <paldavid2030@gmail.com>
2019-06-28 10:49:14 +0300

    add retry

--- chapter_eight.js
+++ chapter_eight.js
@@ -1,2 +1,24 @@
 //bugs and errors
 
+class MultiplicatorUnitFailure extends Error { }
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  try {
+    return primitiveMultiply(a, b);
+  } catch (e) {
+    if (e instanceof MultiplicatorUnitFailure) {
+      return reliableMultiply(a, b);
+    };
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
