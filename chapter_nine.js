f4f0c1a8205af2fb3f8989a30db5ca735071f2f8
David Pal <paldavid2030@gmail.com>
2019-07-01 14:32:43 +0300

    start excersise regexp golf

--- chapter_nine.js
+++ chapter_nine.js
@@ -1,2 +1,17 @@
 //regular expressions
 
+//regexp golf 
+
+// car and cat
+
+const ca = /ca[rt]/;
+
+// pop and prop
+
+const prop = /pr?op/;
+
+// ferret, ferry, ferrari
+
+const ferr = /ferr(et|y|ari)/;
+
+
