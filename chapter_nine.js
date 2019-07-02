7a94878d7dc4ae92730cd4f432f84cf9b90d70fe
David Pal <paldavid2030@gmail.com>
2019-07-02 17:22:55 +0300

    improve code formatting

--- chapter_nine.js
+++ chapter_nine.js
@@ -33,13 +33,13 @@ let number = /^[+-]?((\.\d+\b)$|(\d+\.?\d*)(([eE][+-]?)?\d+)?$)/;
 
 // Tests - from the book:
 for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
-                 "1.3e2", "1E-4", "1e+12"]) {
+  "1.3e2", "1E-4", "1e+12"]) {
   if (!number.test(str)) {
     console.log(`Failed to match '${str}'`);
   }
 }
 for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
-                 ".5.", "1f5", "."]) {
+  ".5.", "1f5", "."]) {
   if (number.test(str)) {
     console.log(`Incorrectly accepted '${str}'`);
   }
