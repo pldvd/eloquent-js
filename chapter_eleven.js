8f1015ca59f707008e3cc9b8b72e7d7ef4f1b976
David Pal <paldavid2030@gmail.com>
2019-07-09 13:44:16 +0300

    add generator function

--- chapter_eleven.js
+++ chapter_eleven.js
@@ -1,2 +1,19 @@
 // asynchronous programming
 
+
+// generator function
+function* iterator(n) {
+  for (let current = n; current < 50; current*=n) {
+    yield current;
+  }
+}
+
+for (let current of iterator(3)){
+  console.log(current);
+}
+
+const it = iterator(3)[Symbol.iterator]();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
