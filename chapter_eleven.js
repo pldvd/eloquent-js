38ebae6c907dc9a89d3877df3192d5f6ccd90cad
David Pal <paldavid2030@gmail.com>
2019-07-11 12:47:33 +0300

    add solution of promise.all

--- chapter_eleven.js
+++ chapter_eleven.js
@@ -3,12 +3,12 @@
 
 // generator function
 function* iterator(n) {
-  for (let current = n; current < 50; current*=n) {
+  for (let current = n; current < 50; current *= n) {
     yield current;
   }
 }
 
-for (let current of iterator(3)){
+for (let current of iterator(3)) {
   console.log(current);
 }
 
@@ -22,9 +22,26 @@ console.log(it.next());
 
 async function locateScalpel(nest) {
   let current = nest.name;
-  while(nest) {
+  while (nest) {
     let next = await anyStorage(nest, current, "scalpel");
     if (next == current) return current;
     current = next;
   }
+}
+
+//building promise.all
+
+function promise_all(promises) {
+  return new Promise((resolve, reject) => {
+    let returnVal = [];
+    let promiseCount = promises.length;
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(value => {
+        returnVal[i] = value;
+        promiseCount -= 1;
+        if (promiseCount == 0) resolve(returnVal);
+      }).catch(e => reject(e));
+    }
+    if (promises.length == 0) resolve(returnVal);
+  })
 }
