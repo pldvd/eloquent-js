0d02e5f96631c0c3343572311052073d0b204316
David Pal <paldavid2030@gmail.com>
2019-07-11 12:48:38 +0300

    add test code to build promise_all

--- chapter_eleven.js
+++ chapter_eleven.js
@@ -31,7 +31,7 @@ async function locateScalpel(nest) {
 
 //building promise.all
 
-function promise_all(promises) {
+function Promise_all(promises) {
   return new Promise((resolve, reject) => {
     let returnVal = [];
     let promiseCount = promises.length;
@@ -45,3 +45,24 @@ function promise_all(promises) {
     if (promises.length == 0) resolve(returnVal);
   })
 }
+
+
+// Test code.
+Promise_all([]).then(array => {
+  console.log("This should be []:", array);
+});
+function soon(val) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
+Promise_all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
+Promise_all([soon(1), Promise.reject("X"), soon(3)]).then(array => {
+  console.log("We should not get here");
+}).catch(error => {
+  if (error != "X") {
+    console.log("Unexpected failure:", error);
+  }
+});
