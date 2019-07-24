c1ed8b8f2d148ceb69408d69c54fa3e3aac489b6
David Pal <paldavid2030@gmail.com>
2019-07-24 17:21:28 +0300

    fetch text/plain resource

--- chapter_eigtheen.js
+++ chapter_eigtheen.js
@@ -1,1 +1,11 @@
 //HTTP and forms
+
+//content negotiation
+
+//fetch text/plain
+fetch('https://eloquentjavascript.net/author', {headers: {
+  'accept': 'text/plain'
+}})
+.then(response => response.text())
+.then(text => console.log(text));
+
