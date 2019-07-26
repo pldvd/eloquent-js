808bda09fa9086578730f8df2ba0c035cb51ebd3
David Pal <paldavid2030@gmail.com>
2019-07-26 09:50:41 +0300

    create cell objects for each checkbox with cell location on grid as property

--- chapter_eigtheen_game_of_life/script.js
+++ chapter_eigtheen_game_of_life/script.js
@@ -12,8 +12,11 @@
             for (let i = 0; i < this.width; i++) {
               let row = [];
               for (let y = 0; y < this.height; y++) {
-                9
-                row.push(Math.random() > 0.5);
+                let cell = {
+                  position: [i, y],
+                  isAlive: Math.random() > 0.5
+                }
+                row.push(cell);
               }
               matrix.push(row);
             }
@@ -22,61 +25,61 @@
         }
       }
 
-      checkNeighbours() {
-        this.content.map((row, rowIndex) => {
+    //   checkNeighbours() {
+    //     this.content.map((row, rowIndex) => {
 
-          row.map((cell, cellIndex) => {
-            let aliveNeighbours = 0;
-            let deadNeighbours = 0;
-            let newRow = [];
+    //       row.map((cell, cellIndex) => {
+    //         let aliveNeighbours = 0;
+    //         let deadNeighbours = 0;
+    //         let newRow = [];
 
-            const neighbourCells = [
-              [rowIndex - 1, cellIndex - 1],
-              [rowIndex - 1, cellIndex],
-              [rowIndex - 1, cellIndex + 1],
-              [rowIndex, cellIndex - 1],
-              [rowIndex, cellIndex + 1],
-              [rowIndex + 1, cellIndex - 1],
-              [rowIndex + 1, cellIndex],
-              [rowIndex + 1, cellIndex + 1]
-            ];
+    //         const neighbourCells = [
+    //           [rowIndex - 1, cellIndex - 1],
+    //           [rowIndex - 1, cellIndex],
+    //           [rowIndex - 1, cellIndex + 1],
+    //           [rowIndex, cellIndex - 1],
+    //           [rowIndex, cellIndex + 1],
+    //           [rowIndex + 1, cellIndex - 1],
+    //           [rowIndex + 1, cellIndex],
+    //           [rowIndex + 1, cellIndex + 1]
+    //         ];
 
-            let filteredForMissing = neighbourCells.map(neighbour => {
-              if (neighbour[0] >= 0 && neighbour[1] >= 0 && neighbour[0] < this.content.length && neighbour[1] < row.length) {
-                return this.content[neighbour[0]][neighbour[1]];
-              } else {
-                return null;
-              }
-            })
+    //         let filteredForMissing = neighbourCells.map(neighbour => {
+    //           if (neighbour[0] >= 0 && neighbour[1] >= 0 && neighbour[0] < this.content.length && neighbour[1] < row.length) {
+    //             return this.content[neighbour[0]][neighbour[1]];
+    //           } else {
+    //             return null;
+    //           }
+    //         })
 
-            filteredForMissing.forEach(elem => {
-              if (elem) {
-                aliveNeighbours += 1;
-              } else deadNeighbours += 1;
-            });
+    //         filteredForMissing.forEach(elem => {
+    //           if (elem) {
+    //             aliveNeighbours += 1;
+    //           } else deadNeighbours += 1;
+    //         });
 
-            // if (aliveNeighbours < 2 || aliveNeighbours > 3) {
-            //   newRow[cellIndex] = false;
-            //   // console.log('a');
-            // } else if (cell === true && 2 <= aliveNeighbours <= 3) {
-            //   // console.log('b');
-            //   newRow[cellIndex] = cell;
-            // } else if (cell === false && aliveNeighbours === 3) {
-            //   // console.log('c');
-            //   newRow[rowIndex] = true;
-            // } else {
-            //   newRow[rowIndex] = cell;
-            // }
+    //         // if (aliveNeighbours < 2 || aliveNeighbours > 3) {
+    //         //   newRow[cellIndex] = false;
+    //         //   // console.log('a');
+    //         // } else if (cell === true && 2 <= aliveNeighbours <= 3) {
+    //         //   // console.log('b');
+    //         //   newRow[cellIndex] = cell;
+    //         // } else if (cell === false && aliveNeighbours === 3) {
+    //         //   // console.log('c');
+    //         //   newRow[rowIndex] = true;
+    //         // } else {
+    //         //   newRow[rowIndex] = cell;
+    //         // }
 
-            console.log(filteredForMissing, deadNeighbours, aliveNeighbours);
-            console.log(cell);
-            // console.log(newRow);
-            // newMatrix.push(newRow);
+    //         console.log(filteredForMissing, deadNeighbours, aliveNeighbours);
+    //         console.log(cell);
+    //         // console.log(newRow);
+    //         // newMatrix.push(newRow);
 
 
-          })
-        })
-      }
+    //       })
+    //     })
+    //   }
     }
 
     function drawGeneration(matrix) {
@@ -87,14 +90,15 @@
         for (let y = 0; y < matrix.height; y++) {
           const checkboxNode = document.createElement('input');
           checkboxNode.type = 'checkbox';
-          checkboxNode.checked = matrix.content[i][y];
+          checkboxNode.checked = matrix.content[i][y].isAlive;
           newRow.appendChild(checkboxNode);
         }
         gridNode.appendChild(newRow);
       }
+
+      //console.log(matrix.content);
     }
 
 
     let myMatrix = new Matrix(5, 5);
-    myMatrix.checkNeighbours();
     drawGeneration(myMatrix);
