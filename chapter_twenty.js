3110584f816915741920b9f048a609f04ea254f2
David Pal <paldavid2030@gmail.com>
2019-07-29 17:31:08 +0300

    add first solution to search tool

--- chapter_twenty.js
+++ chapter_twenty.js
@@ -2,3 +2,18 @@
 
 // search tool
 
+const fs = require('fs');
+
+const regexp = new RegExp(process.argv[2]);
+const filesToRead = process.argv.slice(3);
+
+filesToRead.map(file => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err.message);
+      return null;
+    }
+    regexp.test(data) ? console.log(file) : console.log('not found');
+  })
+})
+
