f5bdfef45fd638dd67233251d02306e83eacbe0d
David Pal <paldavid2030@gmail.com>
2019-07-31 09:19:07 +0300

    check weather file is a directory and invoke same function recursively if it is

--- chapter_twenty.js
+++ chapter_twenty.js
@@ -7,12 +7,20 @@ const fsPromises = require('fs').promises;
 const regexp = new RegExp(process.argv[2]);
 const filesToRead = process.argv.slice(3);
 
-filesToRead.forEach(async file => {
+async function searchFileContent(file) {
   try {
-    const fileContent = await fsPromises.readFile(file, 'utf8');
-    regexp.test(fileContent) && console.log(file);
+    const fileIsDirectory = await fsPromises.stat(file);
+    if (fileIsDirectory.isDirectory()) {
+      const dirContent = await fsPromises.readdir(file);
+      dirContent.forEach(dirFile => searchFileContent(dirFile));
+    } else {
+      const fileContent = await fsPromises.readFile(file, 'utf8');
+      regexp.test(fileContent) && console.log(file);
+    };
   }
   catch (e) {
     console.log(e.message);
   }
-})
+}
+
+filesToRead.forEach(file => searchFileContent(file));
