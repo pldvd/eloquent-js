7487c0093311f75dd77f7d977f9f00402c30bbaa
David Pal <paldavid2030@gmail.com>
2019-07-31 08:09:41 +0300

    search tool works with callbacks

--- chapter_twenty.js
+++ chapter_twenty.js
@@ -8,12 +8,15 @@ const regexp = new RegExp(process.argv[2]);
 const filesToRead = process.argv.slice(3);
 
 filesToRead.map(file => {
-  fs.readFile(file, 'utf8', (err, data) => {
+  let result = fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
       console.log(err.message);
       return null;
     }
     regexp.test(data) ? console.log(file) : console.log('not found');
-  })
+  });
+  return result;
 })
+
+
 
