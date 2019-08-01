ee70c845a3c4d2bbbd756f99607a75eba01d9869
David Pal <paldavid2030@gmail.com>
2019-08-01 18:08:53 +0300

    use path module to resolve full url to file

--- chapter_twenty.js
+++ chapter_twenty.js
@@ -3,16 +3,16 @@
 // search tool
 
 const fsPromises = require('fs').promises;
-
+const path = require('path');
 const regexp = new RegExp(process.argv[2]);
 const filesToRead = process.argv.slice(3);
 
 async function searchFileContent(file) {
   try {
-    const fileIsDirectory = await fsPromises.stat(file);
-    if (fileIsDirectory.isDirectory()) {
+    const fileStat = await fsPromises.stat(file);
+    if (fileStat.isDirectory()) {
       const dirContent = await fsPromises.readdir(file);
-      dirContent.forEach(dirFile => searchFileContent(dirFile));
+      dirContent.forEach(dirFile => searchFileContent(path.resolve(file, dirFile)));
     } else {
       const fileContent = await fsPromises.readFile(file, 'utf8');
       regexp.test(fileContent) && console.log(file);
